ba9550bc2c9787b661ea69df5b1640eeee504524
Billy Maulana <billymaulana1999@gmail.com>
2023-01-19 19:30:32 +0700

    chore: add secondary env

--- src/composables/fetch.ts
+++ src/composables/fetch.ts
@@ -2,7 +2,7 @@
 import { ofetch } from 'ofetch'
 
 export const useMyFetch = ofetch.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://itunes.apple.com',
   async onRequest({ request }) {
     console.log('fetch request', request)
   },
